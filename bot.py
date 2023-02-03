2730db3ca00954c037227b6d3728ce3bda76fe7a
chamodperera <chamodperera87@gmail.com>
2023-02-03 12:30:13 +0530

    added flask server

--- bot.py
+++ bot.py
@@ -1,12 +1,18 @@
 import os
+from background import keep_alive
 import telebot
 
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 
-#initialize bot
 bot = telebot.TeleBot(BOT_TOKEN)
 
-#welcome message
-@bot.message_handler(commands=['start', 'hello'])
-def send_welcome(message):
-    bot.reply_to(message, "Howdy, how are you doing?")
+#Greeting message
+@bot.message_handler(content_types=['text'])
+def handle_text(message):
+    if message.text == 'Hello':
+        bot.reply_to(message, 'Hello! How can I help you today?')
+
+
+#Server
+keep_alive() 
+bot.polling(non_stop=True, interval=0)
